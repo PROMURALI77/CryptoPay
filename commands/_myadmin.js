41ef40d008f1a894b51e4e5992a1c6a298f83c0e
Samuel <89231917+SamuelDev00@users.noreply.github.com>
2022-03-20 22:37:25 -0400

    Add files via upload

---
+++ commands/_myadmin.js
@@ -0,0 +1,15 @@
+/*CMD
+  command: /myadmin
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+var admin = "1498810624"
+
+
+Bot.setProperty("admin",admin)

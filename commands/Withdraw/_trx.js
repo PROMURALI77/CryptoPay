6278e9d4085ef46e22ea7f26d4f59525b9b55669
Samuel <89231917+SamuelDev00@users.noreply.github.com>
2022-03-21 17:20:01 -0400

    Update _trx.js

--- commands/Withdraw/_trx.js
+++ commands/Withdraw/_trx.js
@@ -13,6 +13,6 @@ var res = Libs.ResourcesLib.userRes("balance");
 if(res.value() < 0.2 ){
 Bot.sendMessage("❌ Cantidad mínima para retirar 0.2 DGB")
 }else{
-Bot.sendKeyboard("⬅️ Atrás","📤 _¿Cuántos BTT quiere retirar?_\n\n*Mínimo:* `0.2 DGB`"+"\n"+"*Máximo:* `"+res.value().toFixed(4)+"`* DGB\n"+"*\n_El monto máximo corresponde a su saldo._"+"\n\n"+"*➡️ Envíe ahora la cantidad que desea retirar*")
+Bot.sendKeyboard("⬅️ Atrás","📤 _¿Cuántos DGB quiere retirar?_\n\n*Mínimo:* `0.2 DGB`"+"\n"+"*Máximo:* `"+res.value().toFixed(4)+"`* DGB\n"+"*\n_El monto máximo corresponde a su saldo._"+"\n\n"+"*➡️ Envíe ahora la cantidad que desea retirar*")
 Bot.runCommand("/withtrx")
 }

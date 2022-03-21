9841de4c498f17ecc3ac7af2e5966aaea153db49
Samuel <89231917+SamuelDev00@users.noreply.github.com>
2022-03-21 13:55:29 -0400

    Update _trxprice.js

--- commands/Other Buttons/_trxprice.js
+++ commands/Other Buttons/_trxprice.js
@@ -18,7 +18,7 @@ var result = JSON.parse(data.content);
 var TRX = result.market_data.current_price.usd
 var time = Libs.DateTimeFormat.format(new Date(),"dd/m/yyyy h:M:s T")+"M"
 Api.sendPhoto({
-photo:"https://www.tecnologia.press/wp-content/uploads/2019/06/1_BYXFfsRJZ_hyMrwtq4y-bg.png",
+photo:"https://su-link.herokuapp.com/dl/0/AgADZKoxG_KoyUU.jpg",
 caption:"📈 *Precio del BTT hoy:* \n1 BTT= $ `" + TRX.toFixed(8) + "`\n\n" + "*🕓 Hora del Servidor:*" +"\n" +time+"" + "\n\n" + "🌐 Canal de pago en vivo: "+channel+ "",parse_mode : "Markdown" , disable_web_page_preview: true
 });
 
